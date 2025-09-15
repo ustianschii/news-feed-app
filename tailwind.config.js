510387136b697eab97ece21e7e6988d4adde62d8
ustianschii <ustianschii@gmail.com>
2025-09-15 21:30:59 +0300

    chore: initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#f5f7ff',
+          500: '#2563eb'
+        }
+      }
+    }
+  },
+  plugins: []
+}
