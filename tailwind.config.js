c205449b8b8c3e88e35efa1105779f8978220f19
ustianschii <ustianschii@gmail.com>
2025-09-16 19:09:06 +0300

    chore: add vite plugins and fix biome config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{ts,tsx}"
-  ],
-  theme: {
-    extend: {
-      colors: {
-        brand: {
-          50: '#f5f7ff',
-          500: '#2563eb'
-        }
-      }
-    }
-  },
-  plugins: []
-}
+	content: ["./index.html", "./src/**/*.{ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				brand: {
+					50: "#f5f7ff",
+					500: "#2563eb",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
+import viteCompression from "vite-plugin-compression";
+import inspect from "vite-plugin-inspect";
+import svgr from "vite-plugin-svgr";
+import vitePluginVirtual from "vite-plugin-virtual";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		svgr(),
+		inspect(),
+		checker({
+			typescript: true,
+			biome: true,
+		}),
+		viteCompression({
+			algorithm: "gzip",
+		}),
+		visualizer({
+			open: true,
+			gzipSize: true,
+		}),
+		vitePluginVirtual({
+			"virtual:my-module": `export const message = 'Hello from a virtual module!';`,
+		}),
+	],
+	build: {
+		minify: "terser",
+		terserOptions: {},
+	},
+});
